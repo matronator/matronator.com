6bb5bbd885a801dd0ddb12a2630d63e0d97688e5
Matronator <5470780+matronator@users.noreply.github.com>
2022-06-26 02:18:00 +0200

    modernize building etc (add parcel...)

--- get-locale.php
+++ get-locale.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace App;
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+
 if (!empty($_POST)) {
     $lang = $_POST['lang'] ?? null;
     if ($lang === null) {
@@ -12,7 +18,8 @@ if (!empty($_POST)) {
         $locale = trim(strtolower($match[0]));
     }
 
-    $strings = json_decode(file_get_contents(__DIR__ . "/locale/$locale.json"));
+    $strings = Yaml::parseFile(__DIR__ . "/locale/$locale.yaml");
+    // $strings = json_decode(file_get_contents(__DIR__ . "/locale/$locale.json"));
     header("Content-Type: application/json");
     echo json_encode($strings);
     die();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import baguetteBox from "baguettebox.js";
+
 document.addEventListener(`DOMContentLoaded`, () => {
     pageLoaded()
 
